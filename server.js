90a95eb5583b9daaff6787f440d527199240305c
Ryan Coppolo <r@coppolo.com>
2012-10-08 15:36:05 -0700

    Adds /public and imports a stylesheet.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
   res.render('hello');
