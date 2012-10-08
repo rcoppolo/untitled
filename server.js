b5f5a3379accb0ecd2ee56526c6960eb9686668e
Ryan Coppolo <r@coppolo.com>
2012-10-08 13:12:33 -0700

    Very simple template render w/ jade.

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine', 'jade');
+
 app.get('/', function(req, res){
-  res.send('hello.');
+  res.render('hello');
 });
 
 app.listen(8080);
