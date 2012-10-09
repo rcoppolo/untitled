d0be16fc12df153c31493768a0952ce9a9ad7a2a
Ryan Coppolo <r@coppolo.com>
2012-10-08 21:03:14 -0700

    Installs stylus.

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express');
+var stylus = require('stylus');
 
 var app = express();
 
 app.set('view engine', 'jade');
+
+app.use(stylus.middleware({
+  src: __dirname + '/public/stylesheets',
+  dest: __dirname + '/public'
+}));
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
