38c534bbd193f0ddb2ade6cbd2ef71b5e15756db
Ryan Coppolo <r@coppolo.com>
2012-10-11 09:41:01 -0700

    Halfway sets up testing environment.

---
+++ test/array.js
@@ -0,0 +1,8 @@
+var should = require("should");
+
+describe('Addition', function() {
+  it('should value numbers with integrity', function() {
+    var one = 1;
+    one.should.equal(1);
+  });
+});
