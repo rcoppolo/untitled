2e40431f5bc4ccf203107189c36752e0069701d8
Ryan Coppolo <r@coppolo.com>
2012-10-10 22:13:38 -0700

    Installs mongoose and sets up mix model.

--- server.js
+++ server.js
@@ -3,15 +3,36 @@ var express = require('express');
 var stylus = require('stylus');
 var requirejs = require('requirejs');
 
+var mongoose = require('mongoose');
+var db = mongoose.createConnection('localhost', 'untitled');
+
+var mixSchema = new mongoose.Schema({
+  title: String,
+  locked: Boolean,
+  plays: Number
+});
+
+var Mix = db.model('Mix', mixSchema);
+
+var mix = new Mix({
+  title: "8ctober",
+  locked: false
+});
+
+mix.save(function(err) {
+  if(!err) {
+    return console.log("Created a mix.");
+  }
+});
+
 var config = {
   baseUrl: __dirname + '/public/javascripts',
   name: 'main',
   out: __dirname + '/public/main.js'
 };
-
 requirejs.optimize(config, function(buildResponse) {
   var contents = fs.readFileSync(config.out, 'utf8');
-  console.log("...javascripts compiled by require.js");
+  console.log("Javascripts compiled by require.js.");
 });
 
 var app = express();
@@ -28,6 +49,12 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
   res.render('hello');
+});
+
+app.get('/api/mixes', function(req, res) {
+  return Mix.find(function(err, mixes) {
+    return res.send(mixes);
+  });
 });
 
 app.listen(8080);
