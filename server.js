63d0c6425f223e02555cd9b6fe5ec9ebd646d5a4
Ryan Coppolo <r@coppolo.com>
2012-10-11 09:17:32 -0700

    Untested restful api.

--- server.js
+++ server.js
@@ -47,13 +47,59 @@ app.use(stylus.middleware({
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.render('hello');
 });
 
 app.get('/api/mixes', function(req, res) {
   return Mix.find(function(err, mixes) {
     return res.send(mixes);
+  });
+});
+
+app.get('api/mixes/:id', function(req, res) {
+  return Mix.findById(req.params.id, function(err, mix) {
+    if(!err) {
+      return res.send(mix);
+    }
+  });
+});
+
+app.post('/api/mixes', function(req, res) {
+  var mix = new Mix({
+    title: req.body.title,
+    locked: req.body.locked
+  });
+  mix.save(function(err) {
+    if(!err) {
+      return console.log("Created " + mix.title);
+    }
+  });
+  return res.send(mix);
+});
+
+app.put('/api/mixes/:id', function(req, res) {
+  return Mix.findById(req.params.id, function(err, mix) {
+    mix.title = req.body.title;
+    mix.locked = req.body.locked;
+    return todo.save(function(err) {
+      if(!err) {
+        console.log("Updated " + mix.title);
+      }
+      return res.send(mix);
+    });
+  });
+});
+
+app.delete('/api/mixes/:id', function(req, res) {
+  return Mix.findById(req.params.id, function(err, mix) {
+    var titleDeleted = mix.title;
+    return mix.remove(function(err) {
+      if(!err) {
+        console.log("Removed " + titleDeleted);
+        return res.send('');
+      }
+    });
   });
 });
 
