b1390f8880a0c46c4749174fdce4a482443888cd
Ryan Coppolo <r@coppolo.com>
2012-10-16 23:22:06 -0700

    No longer auto creates a mix on boot up.

--- server.js
+++ server.js
@@ -5,25 +5,12 @@ var requirejs = require('requirejs');
 var mongoose = require('mongoose');
 
 var db = mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost/untitled");
-
 var mixSchema = new mongoose.Schema({
   title: String,
   locked: Boolean,
   plays: Number
 });
-
 var Mix = db.model('Mix', mixSchema);
-
-var mix = new Mix({
-  title: "8ctober",
-  locked: false
-});
-
-mix.save(function(err) {
-  if(!err) {
-    return console.log("Created a mix.");
-  }
-});
 
 var config = {
   baseUrl: __dirname + '/public/javascripts',
@@ -34,17 +21,13 @@ requirejs.optimize(config, function(buildResponse) {
   var contents = fs.readFileSync(config.out, 'utf8');
   console.log("Javascripts compiled by require.js.");
 });
-
 var app = express();
-
 app.set('view engine', 'jade');
-
 app.use(stylus.middleware({
   src: __dirname + '/public/stylesheets',
   dest: __dirname + '/public',
   // compress: true
 }));
-
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
