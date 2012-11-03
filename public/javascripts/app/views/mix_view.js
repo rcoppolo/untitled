e1fecdd1c15d484ea804f77c376c744ff293a6dc
Ryan Coppolo <r@coppolo.com>
2012-11-03 12:51:15 -0700

    Adds delete button that removes view, need to create a mix view template.

--- public/javascripts/app/views/mix_view.js
+++ public/javascripts/app/views/mix_view.js
@@ -6,16 +6,32 @@ define(['backbone', 'views/song_view', 'models/song'], function(Backbone, SongVi
 
     initialize: function() {
       this.model.on('change', this.render, this);
+      this.model.on('destroy', this.remove, this);
+    },
+
+    events: {
+      'click .delete': 'deleteMix',
+      'click .confirm': 'reallyDelete'
     },
 
     render: function() {
       var $this = this;
       $($this.el).empty();
-      this.model.collection.forEach(function(model) {
+      $($this.el).append("<span class='delete'>Delete this mix.</span>");
+      this.model.songs.models.forEach(function(model) {
         var song_view = new SongView({ model: model });
         $($this.el).append(song_view.el);
       });
       return this;
+    },
+
+    deleteMix: function(e) {
+      $(e.currentTarget).text("Really?").addClass("confirm");
+    },
+
+    reallyDelete: function() {
+      this.model.destroy();
+      Backbone.history.navigate("/", {'trigger': true});
     }
 
   });
